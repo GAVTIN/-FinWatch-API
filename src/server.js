b22ed7248313d1fe2b15beb2be0634507393a82b
Gaurav Kumar Sinha <gauravkumarsinhasinha@gmail.com>
2026-05-18 14:49:42 +0530

    feat: add config loader with env validation and Express entry point

---
+++ src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+const config = require('./config/env');
+app.listen(config.port, () => {
+    console.log(`Server running on port ${config.port} in ${config.nodeEnv} mode`);
+});
